36973c0ce42bc29e57608c6105b69b94e58b7d18
Marius Dörbandt <git.marius@doerbandt.de>
2019-10-06 18:31:50 +0200

    added zoom_changed signal

--- Scenes/Camera.gd
+++ Scenes/Camera.gd
@@ -3,16 +3,19 @@ extends Camera2D
 var speed = 200
 var scaleFactor = 0.1
 
+signal zoom_changed
+
 func _process(delta: float) -> void:
 	var pos = get_viewport().get_mouse_position()
 	var rect = get_viewport_rect()
 	var center = (rect.position + rect.end) / 2
 	if not rect.grow(-50).has_point(pos):
 		position += (pos - center).normalized() * speed * zoom.x * delta
-		print(zoom.x)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action("ui_scroll_up"):
 		zoom *= exp(-scaleFactor)
+		emit_signal("zoom_changed")
 	if event.is_action("ui_scroll_down"):
 		zoom *= exp(scaleFactor)
+		emit_signal("zoom_changed")
