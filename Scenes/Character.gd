b2c94cb81bfc3192c45d628d89f865354ea50758
Marius Dörbandt <git.marius@doerbandt.de>
2019-10-06 14:54:57 +0200

    added finished flag

--- Scenes/Character.gd
+++ Scenes/Character.gd
@@ -5,12 +5,21 @@ func setText(text: String):
 
 var target = position
 var speed = 100
+var finished = true
+
+func setPos(pos: Vector2):
+	position = pos
+	finished = false
 
 func setTarget(pos: Vector2):
 	target = pos
+	finished = false
 
 func _physics_process(delta: float) -> void:
+	if finished:
+		return
 	if (target - position).length() < speed * delta:
 		position = target
+		finished = true
 	else:
 		position += (target - position).normalized() * speed * delta
