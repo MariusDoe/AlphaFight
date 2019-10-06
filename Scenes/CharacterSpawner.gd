7072804266cbe5d524b0e117bdbb054c5455523b
Marius Dörbandt <git.marius@doerbandt.de>
2019-10-06 14:54:33 +0200

    added word spawning

--- Scenes/CharacterSpawner.gd
+++ Scenes/CharacterSpawner.gd
@@ -1,13 +1,33 @@
 extends Node
 
-onready var Character = preload("res://Scenes/Character.tscn")
+var Character = preload("res://Scenes/Character.tscn")
 
-var characters = []
+var wordsFilePath = "res://Data/all.txt"
 
 func _ready() -> void:
-	for i in range(0, 26):
-		var c = Character.instance()
-		c.setText(char(i + 65))
-		c.setTarget(Vector2(randf() * get_viewport().get_visible_rect().size.x, randf() * get_viewport().get_visible_rect().size.x))
-		add_child(c)
-		characters.append(c)
+	randomize()
+	var wordsFile = File.new()
+	wordsFile.open(wordsFilePath, File.READ)
+	var words = wordsFile.get_as_text().split("\n")
+	for i in range(20):
+		spawnWord(words[randi() % words.size()], pointToRect(randomPointInRect(Rect2(-10000, -10000, 20000, 20000)), 50))
+
+func pointToRect(point: Vector2, size: float):
+	return Rect2(point, point).grow(size)
+
+func stringToIndex(s: String):
+	return s.to_ascii()[0] - 65
+
+func spawnWord(word: String, rect: Rect2):
+	for i in range(word.length()):
+		spawnCharacter(stringToIndex(word[i]), randomPointInRect(rect), randomPointInRect(rect))
+
+func spawnCharacter(index: int, pos: Vector2, target: Vector2):
+	var c = Character.instance()
+	c.setText(char(index + 65))
+	c.setPos(pos)
+	c.setTarget(target)
+	add_child(c)
+
+func randomPointInRect(rect: Rect2):
+	return Vector2(rand_range(rect.position.x, rect.end.x), rand_range(rect.position.y, rect.end.y))
